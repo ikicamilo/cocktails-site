39d18b593afcba3363a10ec185bbdc8e76783be0
ikicamilo <ikicamilo@gmail.com>
2023-05-28 14:53:00 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, cum
+        soluta accusamus et totam nam adipisci est, provident itaque optio quo
+        aperiam odit voluptate. Esse minus, quibusdam tenetur laborum recusandae
+        dolorum corporis amet ea repellendus voluptas cupiditate similique
+        quaerat a.
+      </p>
+    </section>
+  );
+};
+
+export default About;
